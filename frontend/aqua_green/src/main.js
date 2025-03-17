1d3f69d4521f82320523e21b5a47a1888baf3d6f
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2025-03-17 22:44:05 +0700

    feat: adjust folder structure

--- frontend/aqua_green/src/main.js
+++ frontend/aqua_green/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify' // Correctly importing vuetify plugin
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+
+app.mount('#app')
