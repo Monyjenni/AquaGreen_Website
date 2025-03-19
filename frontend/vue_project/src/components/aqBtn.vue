1ad633e02b60a094f401e288244818bab087be74
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2025-03-19 18:27:04 +0700

    feat: Scaffold new project

---
+++ frontend/vue_project/src/components/aqBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-btn :color="color" rounded="xl" size="large" class="text-none" block> 
+      {{ label }}
+    </v-btn>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: {
+    type: String,
+    default: '#E84D72'
+  },
+  label: {
+    type: String,
+    required: true
+  }
+});
+</script>
---
+++ frontend/vue_project/src/components/aqCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.card {
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(124, 123, 123, 0.1);
+  padding: 20px;
+}
+</style>
